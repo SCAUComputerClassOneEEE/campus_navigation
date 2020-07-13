043231114198748d88de3156f52104fc03a66784
hiluyx <luyx27877@163.com>
2020-07-13 10:55:26 +0800

    2020.7.13 DataTab注释和loader.fxml的更改

--- src/main/java/com/scaudachuang/campus_navigation/fx/controller/DataTab.java
+++ src/main/java/com/scaudachuang/campus_navigation/fx/controller/DataTab.java
@@ -11,36 +11,48 @@ import javafx.scene.control.TableView;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.List;
 
 public class DataTab<E> extends Tab {
+    //泛型类
     private final Class<?> EType;
+    //TableView动态数据
     private final ObservableList<E> eObservableList;
-    private final List<TableColumn<E,String>> tableColumns = new ArrayList<>();
+    //TableView
+    private final TableView<E> tableView;
 
     public DataTab(DataEnum.DataForm dataForm, List<E> eList) throws ClassNotFoundException {
-        eObservableList = FXCollections.observableArrayList(eList);
-        TableView<E> tableView = new TableView<>(eObservableList);
-        super.setText(String.valueOf(dataForm));
-        super.setContent(tableView);
+
+        //泛型数据装载
         EType = Class.forName(DataEnum.toUrl(dataForm));
+        //Tab标头
+        super.setText(String.valueOf(dataForm));
+
+        //初始化TableView
+        eObservableList = FXCollections.observableArrayList(eList);
+        tableView = new TableView<>(eObservableList);
+        super.setContent(tableView);
         initTableColumns();
-        tableView.getColumns().addAll(tableColumns);
     }
 
     private void initTableColumns(){
+        //获得泛型类的数据域
         Field[] fields = EType.getDeclaredFields();
-        System.out.println(EType.getName());
         for (Field field : fields){
+            //变量名
             String columnName = field.getName();
+            //表属性
             TableColumn<E,String> eTableColumn = new TableColumn<>(columnName);
-            tableColumns.add(eTableColumn);
+            tableView.getColumns().add(eTableColumn);
+            //方法名，例如：id->getId
             String methodName = "get" + columnName.substring(0, 1).toUpperCase() + columnName.substring(1);
             eTableColumn.setCellValueFactory(param -> {
                 try {
+                    //获得泛型类的实例data
                     Object data = param.getValue();
+                    //获得实例的get方法
                     Method method = param.getValue().getClass().getMethod(methodName);
+                    //反射执行get方法
                     return new SimpleStringProperty(method.invoke(data).toString());
                 } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
                     e.printStackTrace();
